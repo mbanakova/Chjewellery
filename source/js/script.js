d2535b707ddb1f36043bbb4d12578300a704da64
Маша <mbanakova@yandex.ru>
2021-03-27 16:19:17 +0300

    pics & accordion

--- source/js/script.js
+++ source/js/script.js
@@ -5,3 +5,19 @@ let header = document.querySelector("header");
 burger.addEventListener("click", () => {
     header.classList.toggle("header--close");
 });
+
+let items = document.querySelectorAll(".faq__item");
+
+items.forEach((item) => {
+    item.addEventListener("click", () => {
+        const title = item.querySelector(".faq__title");
+        console.log(title);
+        const height = title.nextElementSibling.scrollHeight;
+        title.classList.toggle("faq__title--active");
+        if (title.classList.contains("faq__title--active")) {
+            title.nextElementSibling.style.maxHeight = `${height}px`;
+        } else {
+            title.nextElementSibling.style.maxHeight = 0;
+        }
+    });
+});
