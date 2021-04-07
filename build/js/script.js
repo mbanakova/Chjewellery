1a8d0bde318b3bd31db286600f0710226a37c9c4
Маша <mbanakova@yandex.ru>
2021-04-07 16:52:44 +0300

    js

--- build/js/script.js
+++ build/js/script.js
@@ -5,24 +5,55 @@
   document.querySelector(".header").classList.add("header--close");
   let burger = document.querySelector(".header__burger");
   let header = document.querySelector("header");
-  burger.addEventListener("click", () => {
-    header.classList.toggle("header--close");
-  });
-  let items = document.querySelectorAll(".faq__item");
-  items.forEach(item => {
-    item.addEventListener("click", () => {
-      const title = item.querySelector("h3");
-      console.log(title);
-      const height = title.nextElementSibling.scrollHeight;
-      title.classList.toggle("active");
 
-      if (title.classList.contains("active")) {
-        title.nextElementSibling.style.maxHeight = `${height}px`;
-      } else {
-        title.nextElementSibling.style.maxHeight = 0;
-      }
+  if (burger) {
+    burger.addEventListener("click", () => {
+      header.classList.toggle("header--close");
     });
-  }); /// swper
+  }
+
+  let items = document.querySelectorAll(".faq__item");
+
+  if (items) {
+    items.forEach(item => {
+      item.addEventListener("click", () => {
+        const title = item.querySelector("h3");
+        console.log(title);
+        const height = title.nextElementSibling.scrollHeight;
+        title.classList.toggle("active");
+
+        if (title.classList.contains("active")) {
+          title.nextElementSibling.style.maxHeight = `${height}px`;
+        } else {
+          title.nextElementSibling.style.maxHeight = 0;
+        }
+      });
+    });
+  } // filter
+
+
+  let filter = document.querySelector(".catalog__filter-wrap");
+  let filterToggle = document.querySelector(".catalog__button");
+  let filterClose = document.querySelector(".filter__close");
+
+  if (filter) {
+    filter.classList.add("catalog__filter-wrap--js");
+  }
+
+  if (filterToggle) {
+    filterToggle.addEventListener("click", () => {
+      filter.classList.toggle("catalog__filter-wrap--open");
+      filter.classList.toggle("catalog__filter-wrap--js");
+    });
+  }
+
+  if (filterClose) {
+    filterClose.addEventListener("click", () => {
+      filter.classList.remove("catalog__filter-wrap--open");
+      filter.classList.add("catalog__filter-wrap--js");
+    });
+  } /// swiper
+
 
   let swiper = new Swiper(".swiper-container", {
     slidesPerView: 4,
@@ -40,7 +71,7 @@
       el: ".swiper-pagination",
       clickable: true,
       renderBullet: function (index, className) {
-        return '<span class="' + className + '">' + (index + 1) + "</span>";
+        return `<span class="${className}">${index + 1}</span>`;
       }
     },
     lazy: {
@@ -54,7 +85,7 @@
           type: `fraction`,
 
           renderFraction(currentClass, totalClass, index, total) {
-            return `<span class="` + currentClass + `"type="button">0 ` + index + ` </span>` + ` of ` + `<span class="` + totalClass + `"type="button">0 ` + total + ` </span>`;
+            return `<span class="${currentClass}" type="button">0${index}</span> of <span class="${totalClass}" type="button">0${total}</span>`;
           }
 
         }
@@ -75,7 +106,8 @@
       }
     }
   });
-})();
+})(); //filter
+
 
 let filter = document.querySelector(".filter");
 let filterToggle = document.querySelector(".catalog__button");
@@ -90,14 +122,133 @@ if (filter) {
 
 let tabs = document.querySelectorAll(".tabs__triggers-item");
 let bigImage = document.querySelector(".tabs__content");
-tabs.forEach(tab => {
-  tab.addEventListener("click", event => {
-    console.log(event.target);
-    event.preventDefault();
-    tabs.forEach(elem => elem.classList.remove("tabs__triggers-item--active"));
-    bigImage.src = event.target.src;
-    bigImage.srcset = event.target.srcset;
-    tab.classList.add("tabs__triggers-item--active");
+
+if (tabs) {
+  tabs.forEach(tab => {
+    tab.addEventListener("click", event => {
+      console.log(event.target);
+      event.preventDefault();
+      tabs.forEach(elem => elem.classList.remove("tabs__triggers-item--active"));
+      bigImage.src = event.target.src;
+      bigImage.srcset = event.target.srcset;
+      tab.classList.add("tabs__triggers-item--active");
+    });
   });
-});
+} // add to cart modal
+
+
+let addBtn = document.querySelector(".jewel__add-link");
+let addModal = document.querySelector(".add");
+
+if (addBtn) {
+  addBtn.addEventListener("click", event => {
+    event.preventDefault();
+    addModal.classList.add("add--open");
+  });
+}
+
+if (addModal) {
+  let overlay = document.querySelector(".add__overlay");
+  let closeBtn = document.querySelector("#add-close"); // закрыть через Esc
+
+  window.addEventListener("keydown", event => {
+    if (event.key === "Escape") {
+      if (addModal.classList.contains("add--open")) {
+        event.preventDefault();
+        addModal.classList.remove("add--open");
+
+        window.onscroll = function () {};
+      }
+    }
+  }); // Закрыть по клику снаружи
+
+  overlay.addEventListener("click", elem => {
+    if (elem.target === overlay && elem.target !== addModal) {
+      addModal.classList.remove("add--open");
+
+      window.onscroll = function () {};
+    }
+  }); // закрыть крестиком
+
+  closeBtn.addEventListener("click", () => {
+    addModal.classList.remove("add--open");
+
+    window.onscroll = function () {};
+  });
+} // login modal
+
+
+let loginBtn = document.querySelectorAll(".js-login");
+let loginModal = document.querySelector(".login");
+let storageEmail = "";
+let email = document.querySelector("#user-email");
+let isStorageSupport = true;
+
+if (loginBtn) {
+  loginBtn.forEach(btn => {
+    btn.addEventListener("click", event => {
+      event.preventDefault();
+      loginModal.classList.add("login--open");
+
+      if (storageEmail) {
+        console.log(email);
+
+        if (email) {
+          console.log(email.value);
+          email.value = storageEmail;
+        }
+      } else {
+        console.log(123);
+        email.focus();
+      }
+    });
+  });
+}
+
+if (loginModal) {
+  let overlay = document.querySelector(".login__overlay");
+  let closeBtn = document.querySelector("#login-close");
+  let loginForm = document.querySelector(".modal-form");
+
+  try {
+    storageEmail = localStorage.getItem("email");
+    console.log(storageEmail);
+  } catch (err) {
+    isStorageSupport = false;
+  } // закрыть через Esc
+
+
+  window.addEventListener("keydown", event => {
+    if (event.key === "Escape") {
+      if (loginModal.classList.contains("login--open")) {
+        event.preventDefault();
+        loginModal.classList.remove("login--open");
+
+        window.onscroll = function () {};
+      }
+    }
+  }); // Закрыть по клику снаружи
+
+  overlay.addEventListener("click", elem => {
+    if (elem.target === overlay && elem.target !== loginModal) {
+      loginModal.classList.remove("login--open");
+
+      window.onscroll = function () {};
+    }
+  }); // закрыть крестиком
+
+  closeBtn.addEventListener("click", () => {
+    loginModal.classList.remove("login--open");
+
+    window.onscroll = function () {};
+  }); // запись email при отправке
+
+  if (loginForm) {
+    loginForm.addEventListener("submit", () => {
+      if (isStorageSupport) {
+        localStorage.setItem("email", email.value);
+      }
+    });
+  }
+}
 //# sourceMappingURL=script.js.map
